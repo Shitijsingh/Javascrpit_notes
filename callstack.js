9441b6432c8d743199aea4a499b2e5b3e34df1f5
Shitij Singh <shitij.singh@wemonde.com>
2025-01-20 14:54:18 +0530

    callstack

---
+++ callstack.js
@@ -0,0 +1,31 @@
+var n = 2;
+function square(num) {
+  var ans = num * num;
+  return ans;
+}
+console.log(ans);
+var square2 = square(n);
+var square4 = square(4);
+
+// `
+// Memory creation
+// n : first phase undefine
+// n: second phase show value 2
+// square:{}
+// square2: 4
+// square4: 16
+// `
+// `
+// code creation
+// Memory creation
+// nums 2
+// ans 4
+
+// nums 4
+// ans 16
+//  code creation
+// retrun ans;
+
+// `;
+
+("call stack maintains the order of execution execution contexts");
