ec4bbfde400ced8bad2ebaa43b49108196bee771
Shitij Singh <shitij.singh@wemonde.com>
2025-01-21 14:28:36 +0530

    start

---
+++ hostoing.js
@@ -0,0 +1,13 @@
+// getName();
+// console.log(x);
+// console.log(getName)
+
+var x = 7;
+
+function getName() {
+  console.log("Namaste javascript");
+}
+
+getName();
+console.log(x);
+console.log(getName);
