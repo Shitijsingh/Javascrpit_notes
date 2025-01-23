8f868155085ea4b5bbf8a2b5417b0745f68fff3d
Shitij Singh <shitij.singh@wemonde.com>
2025-01-23 16:37:11 +0530

    let

---
+++ let-const.js
@@ -0,0 +1,10 @@
+// console.log(a)
+// ReferenceError: Cannot access 'a' before initialization
+let a = 100;
+var b = 100;
+// let a= 1000;
+// SyntaxError: Identifier 'a' has already been declared
+// const c;
+// SyntaxError: Missing initializer in const declaration
+// console.log(c)
+console.log(a)
