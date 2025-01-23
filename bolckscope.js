336d4cd0a677a36edcc6d4b41cdb92befb2d287d
Shitij Singh <shitij.singh@wemonde.com>
2025-01-23 17:41:21 +0530

    blockscoer

---
+++ bolckscope.js
@@ -0,0 +1,48 @@
+// if(true){
+//     // compound statement
+//     var a =10;
+//     console.log(a)
+// }This group of multi statement can be used in a place javascript aspect one statement. is called Block
+
+// we can acess inside this block is called block scope. 
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 40;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+
+// shadowing
+// var a = 100;
+// let b = 100;
+// const c = 100;
+// {
+//      var a = 10;
+//      let b = 20;
+//      const c = 40;
+//      console.log(a);
+//      console.log(b);
+//      console.log(c);
+//  }
+
+//  console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+//  let b = 100;
+// function x (){
+//     var a = 10;
+//      let b = 20;
+//      const c = 40
+//      console.log(a);
+//      console.log(b);
+//      console.log(c);
+// }
+// x()
+// console.log(b)
