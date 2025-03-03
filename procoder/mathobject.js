0eaf3f3ef18579ea4b35aeb64f4c8e42204ee108
Shitij Singh <shitij.singh@wemonde.com>
2025-03-03 11:52:21 +0530

    truthy update

---
+++ procoder/mathobject.js
@@ -0,0 +1,57 @@
+/*
+
+Math in Javascript
+
+Mathematical operators
+1. Addition operators: +
+2. Subtraction operators: -
+3. Multiplication operators: *
+4. Division  operators: \
+5. Remainder operators: %
+6. Exponentation operators: **
+
+properties on math object
+.Math.PI
+.Math.SQRT2
+.Math.E
+
+
+Method on Math object
+.Math.sqrt(x)
+.Math.pow(x,y)
+.Math.floor()
+.Math.round()
+.Math.ceil()
+.Math.random()
+freedomcodecamp
+<p>8/0=Infinity and -8/0=I-Infinity </p>
+*/
+
+// const amountShow = 'view'
+
+// console.log(Math.sqrt(32));
+
+// console.log(Math.pow(4, 4))
+
+// console.log(Math.ceil(3.4))
+
+// console.log(Math.floor(4.5))
+
+// console.log(32 ** 2);
+
+// console.log(Math.round(-3.4))
+
+// console.log(Math.random() * 30)
+
+// console.log(Math.floor(Math.random() * 20))
+// console.log(Math.random(10) * 20)
+
+// const width = +prompt("please enter width value width")
+// const height = +prompt("please enter height value")
+
+
+// console.log(width * height)
+
+// document.write(`rectangle Area: ${width * height}`
+
+// )
---
+++ procoder/truthly_falsely.js
@@ -0,0 +1,15 @@
+//Truthy and falsy Value
+
+// console.log(Boolean(10))
+
+// console.log(-Infinity)
+
+// console.log(NaN ,0) only false in number
+
+// String show false
+
+// console.log(Boolean(''))
+
+// console.log(undefined)
+
+// console.log(null)
